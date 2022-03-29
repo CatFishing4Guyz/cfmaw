a53b835827fcd1181fae65f6b057b57d976ed098
Monkeys30 <47145954+Monkeys30@users.noreply.github.com>
2022-03-29 16:18:16 +0800

    It's back

---
+++ cfmaw.py
@@ -0,0 +1,92 @@
+import requests
+import json
+from pprint import pprint
+
+tag = None
+
+class PlayerS1:
+    def __init__(self, tag):
+        self.tag = tag
+        self.url = f"https://api.clashofmagic.cc/magic-s1/Api/Player/%23{tag}"
+
+    def get(self):
+        response = requests.get(url = self.url, headers = {"accept": "application/json"})
+        jsonresponse = response.content
+        jsoninfo = json.loads(response.content)
+
+        #pprint(jsoninfo) # not sure which is better tbh
+        origjson = json.dumps(jsoninfo, indent=0, sort_keys=True)
+        remove_chars = '{[""]},'
+
+        newjson = origjson
+        for chars in remove_chars:
+            newjson = newjson.replace(chars, "")
+
+        return newjson
+
+class PlayerS2:
+    def __init__(self, tag):
+        self.tag = tag
+        self.url = f"https://api.clashofmagic.cc/magic-s2/Api/Player/%23{tag}"
+
+    def get(self):
+        response = requests.get(url = self.url, headers = {"accept": "application/json"})
+        jsonresponse = response.content
+        jsoninfo = json.loads(response.content)
+
+        #pprint(jsoninfo)
+        origjson = json.dumps(jsoninfo, indent=0, sort_keys=True)
+        remove_chars = '{[""]},'
+
+        newjson = origjson
+        for chars in remove_chars:
+            newjson = newjson.replace(chars, "")
+
+        return newjson
+
+class ClanS1:
+    def __init__(self, tag):
+        self.tag = tag
+        self.url = f"https://api.clashofmagic.cc/magic-s1/Api/Clan/%23{tag}"
+
+    def get(self):
+        response = requests.get(url = self.url, headers = {"accept": "application/json"})
+        jsonresponse = response.content
+        jsoninfo = json.loads(response.content)
+
+        #pprint(jsoninfo)
+        origjson = json.dumps(jsoninfo, indent=0, sort_keys=True)
+        remove_chars = '{[""]},'
+
+        newjson = origjson
+        for chars in remove_chars:
+            newjson = newjson.replace(chars, "")
+
+        return newjson
+
+class ClanS2:
+    def __init__(self, tag):
+        self.tag = tag
+        self.url = f"https://api.clashofmagic.cc/magic-s2/Api/Clan/%23{tag}"
+
+    def get(self):
+        response = requests.get(url = self.url, headers = {"accept": "application/json"})
+        jsonresponse = response.content
+        jsoninfo = json.loads(response.content)
+
+        #pprint(jsoninfo)
+        origjson = json.dumps(jsoninfo, indent=0, sort_keys=True)
+        remove_chars = '{[""]},'
+
+        newjson = origjson
+        for chars in remove_chars:
+            newjson = newjson.replace(chars, "")
+
+        return newjson
+"""
+Demo
+playertag = input("Enter a random S2 player tag (hashtag is not needed): \n")
+tag = playertag
+playerS2 = PlayerS2(tag)
+playerS2.get()
+"""
