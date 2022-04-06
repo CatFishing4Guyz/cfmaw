c55d0a6f6b3efcb3aba079d909e1efbb963dfd1b
Monkeys30 <47145954+Monkeys30@users.noreply.github.com>
2022-04-06 21:16:53 +0800

    Bunch of improvements

--- cfmaw.py
+++ cfmaw.py
@@ -11,25 +11,19 @@ class PlayerS1:
         jsonInfo = json.loads(response.content)
 
         createTime = jsonInfo["analytics"]["createTime"]
-        bestTrophies = jsonInfo["avatar"]["commodity"]["achievements"]["bestTrophies"]
+        mainTrophies = str(jsonInfo["avatar"]["mainTrophies"])
         league = jsonInfo["avatar"]["league"]
         leagueIcon = jsonInfo["avatar"]["leagueIcon"]
         name = jsonInfo["avatar"]["name"]
-        townHallLevels = jsonInfo["avatar"]["townHallLevels"]["main"]
-        xp = jsonInfo["avatar"]["xp"]
-        clanTag = jsonInfo["clanTag"]
-
-        # Converting int to str so concatenation is possible
-        bestTrophies = str(bestTrophies)
-        townHallLevels = str(townHallLevels)
-        xp = str(xp)
-        clanTag = str(clanTag)
+        townHallLevels = str(jsonInfo["avatar"]["townHallLevels"]["main"])
+        level = str(jsonInfo["avatar"]["level"])
+        clanTag = str(jsonInfo["clanTag"])
 
         # Storing the league in a variable until I find something to do with it
         league = jsonInfo["avatar"]["league"]
 
-        return "Name: "+name+"\nLeague: "+league+"\nBest Trophies: "+bestTrophies+"\nCreation Time: "+createTime+"\nTown Hall: "+townHallLevels+"\nXP: "+xp+"\nClan Tag: "+clanTag
-
+        return "Name: "+name+"\nLeague: "+league+"\nTrophies: "+mainTrophies+"\nCreation Time: "+createTime+"\nTown Hall: "+townHallLevels+"\nLevel: "+level+"\nClan Tag: "+clanTag
+        
 class PlayerS2:
     def __init__(self, tag):
         self.tag = tag
@@ -40,22 +34,17 @@ class PlayerS2:
         jsonInfo = json.loads(response.content)
 
         createTime = jsonInfo["analytics"]["createTime"]
-        bestTrophies = jsonInfo["avatar"]["commodity"]["achievements"]["bestTrophies"]
+        mainTrophies = str(jsonInfo["avatar"]["mainTrophies"])
         league = jsonInfo["avatar"]["league"]
         leagueIcon = jsonInfo["avatar"]["leagueIcon"]
         name = jsonInfo["avatar"]["name"]
-        townHallLevels = jsonInfo["avatar"]["townHallLevels"]["main"]
-        xp = jsonInfo["avatar"]["xp"]
-        clanTag = jsonInfo["clanTag"]
-
-        bestTrophies = str(bestTrophies)
-        townHallLevels = str(townHallLevels)
-        xp = str(xp)
-        clanTag = str(clanTag)
+        townHallLevels = str(jsonInfo["avatar"]["townHallLevels"]["main"])
+        level = str(jsonInfo["avatar"]["level"])
+        clanTag = str(jsonInfo["clanTag"])
 
         league = jsonInfo["avatar"]["league"]
 
-        return "Name: "+name+"\nLeague: "+league+"\nBest Trophies: "+bestTrophies+"\nCreation Time: "+createTime+"\nTown Hall: "+townHallLevels+"\nXP: "+xp+"\nClan Tag: "+clanTag
+        return "Name: "+name+"\nLeague: "+league+"\nTrophies: "+mainTrophies+"\nCreation Time: "+createTime+"\nTown Hall: "+townHallLevels+"\nLevel: "+level+"\nClan Tag: "+clanTag
 
 class ClanS1:
     def __init__(self, tag):
@@ -67,23 +56,16 @@ class ClanS1:
         jsonInfo = json.loads(response.content)
 
         chatLocales = jsonInfo["info"]["chatLocales"]
-        level = jsonInfo["info"]["level"]
-        mainTrophies = jsonInfo["info"]["mainTrophies"]
+        level = str(jsonInfo["info"]["level"])
+        mainTrophies = str(jsonInfo["info"]["mainTrophies"])
         name = jsonInfo["info"]["name"]
-        requiredTownHallLevel = jsonInfo["info"]["requiredTownHallLevel"]
-        requiredTrophies = jsonInfo["info"]["requiredTrophies"]
+        requiredTownHallLevel = str(jsonInfo["info"]["requiredTownHallLevel"])
+        requiredTrophies = str(jsonInfo["info"]["requiredTrophies"])
         tags = jsonInfo["info"]["tags"]
         inviteType = jsonInfo["info"]["type"]
         warFrequency = jsonInfo["info"]["warFrequency"]
-        xp = jsonInfo["info"]["xp"]
 
-        level = str(level)
-        mainTrophies = str(mainTrophies)
-        requiredTownHallLevel = str(requiredTownHallLevel)
-        requiredTrophies = str(requiredTrophies)
-        xp = str(xp)
-
-        return "Name: "+name+"\nLevel: "+level+"\nXP: "+xp+"\nTrophies: "+mainTrophies+"\nInvite Type: "+inviteType+"\nWar Frequency: "+warFrequency+"\nRequired Trophies: "+requiredTrophies+"\nRequired Town Hall Level: "+requiredTownHallLevel+"\nLanguage: "+chatLocales
+        return "Name: "+name+"\nLevel: "+level+"\nTrophies: "+mainTrophies+"\nInvite Type: "+inviteType+"\nWar Frequency: "+warFrequency+"\nRequired Trophies: "+requiredTrophies+"\nRequired Town Hall Level: "+requiredTownHallLevel+"\nLanguage: "+chatLocales
 
 class ClanS2:
     def __init__(self, tag):
@@ -95,23 +77,16 @@ class ClanS2:
         jsonInfo = json.loads(response.content)
 
         chatLocales = jsonInfo["info"]["chatLocales"]
-        level = jsonInfo["info"]["level"]
-        mainTrophies = jsonInfo["info"]["mainTrophies"]
+        level = str(jsonInfo["info"]["level"])
+        mainTrophies = str(jsonInfo["info"]["mainTrophies"])
         name = jsonInfo["info"]["name"]
-        requiredTownHallLevel = jsonInfo["info"]["requiredTownHallLevel"]
-        requiredTrophies = jsonInfo["info"]["requiredTrophies"]
+        requiredTownHallLevel = str(jsonInfo["info"]["requiredTownHallLevel"])
+        requiredTrophies = str(jsonInfo["info"]["requiredTrophies"])
         tags = jsonInfo["info"]["tags"]
         inviteType = jsonInfo["info"]["type"]
         warFrequency = jsonInfo["info"]["warFrequency"]
-        xp = jsonInfo["info"]["xp"]
 
-        level = str(level)
-        mainTrophies = str(mainTrophies)
-        requiredTownHallLevel = str(requiredTownHallLevel)
-        requiredTrophies = str(requiredTrophies)
-        xp = str(xp)
-
-        return "Name: "+name+"\nLevel: "+level+"\nXP: "+xp+"\nTrophies: "+mainTrophies+"\nInvite Type: "+inviteType+"\nWar Frequency: "+warFrequency+"\nRequired Trophies: "+requiredTrophies+"\nRequired Town Hall Level: "+requiredTownHallLevel+"\nLanguage: "+chatLocales
+        return "Name: "+name+"\nLevel: "+level+"\nTrophies: "+mainTrophies+"\nInvite Type: "+inviteType+"\nWar Frequency: "+warFrequency+"\nRequired Trophies: "+requiredTrophies+"\nRequired Town Hall Level: "+requiredTownHallLevel+"\nLanguage: "+chatLocales
 
 # Remove the triple quotes to see it in action
 """
