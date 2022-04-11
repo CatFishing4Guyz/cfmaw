ac195387ec8f17861de91b46d83018614c84d363
Monkeys30 <47145954+Monkeys30@users.noreply.github.com>
2022-04-11 18:06:16 +0800

    test

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name="cfmaw",
+      version="1.0-beta",
+      description="API wrapper for Clash of Magic",
+      author="Monkeys30",
+      url="https://github.com/Monkeys30/cfmaw")
