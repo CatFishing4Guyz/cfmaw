3bcdbbe603e40c21b2638674fc0cbb85f61a7b6b
Monkeys30 <47145954+Monkeys30@users.noreply.github.com>
2022-04-15 21:41:50 +0800

    Update to work with an API update
    An update to the API will be released soon, this will be to make sure it still works.

--- cfmaw/ClanS1.py
+++ cfmaw/ClanS1.py
@@ -4,7 +4,7 @@ import json
 class ClanS1:
     def __init__(self, tag):
         self.tag = tag
-        self.url = f"https://api.clashofmagic.cc/magic-s1/Api/Clan/{tag}"
+        self.url = f"https://api.clashofmagic.cc/magic-s1/clans/{tag}"
 
     def name(self):
         self.response = requests.get(url = self.url, headers = {"accept": "application/json"})
