4aa169dfeaf07a1ccd43502e4f51de437995ef8b
Monkeys30 <schp4544@gmail.com>
2022-04-21 20:26:52 +0800

    Just some minor stuff with the test.py

--- test.py
+++ test.py
@@ -1,15 +1,22 @@
 import cfmaw 
 
 # Outputs the player name of the tag you entered
-"""
+
 tag = input("Enter a player tag (S2): ")
 playerS2 = cfmaw.PlayerS2(tag)
 print(playerS2.name())
-"""
+
 
 # Outputs the state of a clan, e.g., is it at war?
-"""
+
 tag = input("Enter a clan tag (S2): ")
 warS2 = cfmaw.WarS2(tag)
 print(warS2.state())
-"""
+
+
+# Outputs the clan level
+
+tag = input("Enter a clan tag (S2): ")
+clanS2 = cfmaw.ClanS2(tag)
+print(clanS2.level())
+
