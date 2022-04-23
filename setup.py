cfd734b70d88b13b8d88e8d75452e2d2db8663e6
Monkeys30 <47145954+Monkeys30@users.noreply.github.com>
2022-04-23 18:29:21 +0800

    Once again, I forgot to do this

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="cfmaw",
-      version="1.2",
+      version="1.3",
       description="API wrapper for Clash of Magic",
       author="Monkeys30",
       url="https://github.com/Monkeys30/cfmaw",
